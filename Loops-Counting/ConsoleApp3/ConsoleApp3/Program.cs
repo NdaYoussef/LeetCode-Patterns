4324555ba09d7ce0e163974bedb68a3d090f46ee
NdaYoussef <ndayou99@gmail.com>
2026-04-11 15:02:34 +0200

    add Richest Customer Wealth problem

---
+++ Loops-Counting/ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace ConsoleApp3
+{
+    internal class Program
+    {
+        public static int MaxWealth(int[][] accounts)
+        {
+            int maxWealth = 0;
+            for(int i =0; i< accounts.Length; i++)
+            {
+                int sum = 0;
+                for(int j = 0; j< accounts[i].Length;j++)
+                {
+                    sum += accounts[i][j];
+                }
+                if(sum > maxWealth) 
+                    maxWealth = sum;
+            }
+            return maxWealth;
+        }
+        static void Main(string[] args)
+        {
+            int[][] arr = [[2, 8, 7], [7, 1, 3], [1, 9, 5]];
+            var result = MaxWealth(arr);
+            Console.WriteLine(result);
+
+
+        }
+    }
+}
