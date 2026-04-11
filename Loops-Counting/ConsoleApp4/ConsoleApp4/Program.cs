23c62ee3ba34bf07538c7dacba916b16a9e8f30a
NdaYoussef <ndayou99@gmail.com>
2026-04-11 15:10:18 +0200

    add Number of Good Pairs problem

---
+++ Loops-Counting/ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace ConsoleApp4
+{
+    internal class Program
+    {
+        public static int NumIdenticalPairs(int[] nums)
+        {
+            int counter = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = 0; j < nums.Length; j++)
+                {
+                    if (nums[i] == nums[j] && i < j)
+                        counter++;
+                }
+            }
+            return counter;
+        }
+        static void Main(string[] args)
+        {
+            int[] arr = [1, 2, 3, 1, 1, 3];
+
+            var result = NumIdenticalPairs(arr);
+            Console.WriteLine(result);
+        }
+    }
+}
