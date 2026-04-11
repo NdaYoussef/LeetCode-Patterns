d2a2712a3293949485f1db6842577ef117dbdd5b
NdaYoussef <ndayou99@gmail.com>
2026-04-11 15:26:23 +0200

    add Count of Odd numbers problem

---
+++ Loops-Counting/ConsoleApp5/ConsoleApp5/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace ConsoleApp5
+{
+    internal class Program
+    {
+        public static int CountOdds(int low, int high)
+        {
+            int counter = 0;
+            for (int i = low; i <= high; i++)
+            {
+                if (i % 2 != 0)
+                    counter++;
+            }
+            return counter;
+        }
+        static void Main(string[] args)
+        {
+            int x = 3; 
+            int y = 7;
+
+            var result = CountOdds(x, y);
+            Console.WriteLine(result);
+        }
+    }
+}
