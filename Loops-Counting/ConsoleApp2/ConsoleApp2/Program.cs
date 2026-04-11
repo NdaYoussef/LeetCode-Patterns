3138923d62d8a9d021aaed7ebc5935cb709378a5
NdaYoussef <ndayou99@gmail.com>
2026-04-11 14:40:45 +0200

    add Running Sum of 1D Arr Solutions

--- Loops-Counting/ConsoleApp2/ConsoleApp2/Program.cs
+++ Loops-Counting/ConsoleApp2/ConsoleApp2/Program.cs
@@ -2,9 +2,20 @@
 {
     internal class Program
     {
+        public static int[] RunningSum(int[] nums)
+        {
+            for (int i = 1; i < nums.Length; i++)
+            {
+                nums[i] += nums[i - 1];
+            }
+            return nums;
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int[] arr = [1, 1, 1, 1, 1];
+
+            var result = RunningSum(arr);
+            Console.WriteLine(string.Join(", ",result));
         }
     }
 }
