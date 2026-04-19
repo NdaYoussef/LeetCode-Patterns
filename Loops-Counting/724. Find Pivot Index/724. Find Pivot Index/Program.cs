319df9ff9890eb025b9572028af99fe7d6394144
NdaYoussef <ndayou99@gmail.com>
2026-04-19 09:27:34 +0200

    add find pivot index

---
+++ Loops-Counting/724. Find Pivot Index/724. Find Pivot Index/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _724._Find_Pivot_Index
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
