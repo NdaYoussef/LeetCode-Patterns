0957f58e6e82a45003f75162dbc46fa652c65cf2
NdaYoussef <ndayou99@gmail.com>
2026-04-19 21:39:31 +0200

    add Defuse the Bomb

---
+++ Sliding-Window/1652. Defuse the Bomb/1652. Defuse the Bomb/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace _1652._Defuse_the_Bomb
+{
+    internal class Program
+    {
+            public static int[] Decrypt(int[] code, int k)
+            {
+                int sum = 0;
+                for (int i = 0; i < k; i++)
+                {
+                    if (k > 0)
+                    {
+                        sum += code[i + 1];
+                        code[i] = sum;
+                    }
+                    else if (k < 0)
+                    {
+                        sum += code[i - 1];
+                        code[i] = sum;
+                    }
+                    else
+                    {
+                        code[i] = 0;
+                    }
+                }
+                return code;
+            }
+        static void Main(string[] args)
+        {
+            int[] code = [5, 7, 1, 4];
+            int k = 3;
+            Console.WriteLine(Decrypt(code,k));
+        }
+    }
+}
