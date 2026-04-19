2641283d62c652be2d72640404fd6e2d1cb470ff
NdaYoussef <ndayou99@gmail.com>
2026-04-19 09:28:01 +0200

    add sliding window pattern

---
+++ Sliding-Window/643. Maximum Average Subarray I/643. Maximum Average Subarray I/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _643._Maximum_Average_Subarray_I
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
