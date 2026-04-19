f220da4db3a050bb0b1c0f6c583b425dbe889afc
NdaYoussef <ndayou99@gmail.com>
2026-04-19 19:50:53 +0200

    add Maximum Average Subarray

--- Sliding-Window/643. Maximum Average Subarray I/643. Maximum Average Subarray I/Program.cs
+++ Sliding-Window/643. Maximum Average Subarray I/643. Maximum Average Subarray I/Program.cs
@@ -2,9 +2,31 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+        public static double FindMaxAverage(int[] nums, int k)
         {
-            Console.WriteLine("Hello, World!");
+            int sum = 0;
+            for(int i =0;i<k;i++)
+            {
+                sum+= nums[i];
+            }
+
+            int maxSum = sum; 
+            for(int i = k; i<nums.Length;i++)
+            {
+                sum += nums[i];
+                sum-= nums[i-k];
+                if(sum > maxSum)
+                    maxSum = sum;
+            }
+            return (double)maxSum / k;
+        }
+
+            static void Main(string[] args)
+        {
+            int[] nums = [1, 12, -5, -6, 50, 3];
+            int k = 4;
+           double result = FindMaxAverage(nums, k);
+            Console.WriteLine($"result: {result}");
         }
     }
 }
